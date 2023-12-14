82684401eddf86d45d919c076c0270a9dfe5aee1
Daniel Moss <daniel.moss@hyperluminalgames.com>
2023-12-14 17:16:46 +0000

    Refactor and massively optimise, solve part 2

--- JavaScript/day11.js
+++ JavaScript/day11.js
@@ -1,30 +1,26 @@
 const fs = require('fs');
-String.prototype.splice = function(start, delCount, newSubStr) {
-    return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
-};
-
 
 let galaxyLines = fs.readFileSync('./day11input1.txt').toString('utf-8').split('\r\n');
 
 let galaxies = [];
-let shortestPaths = [];
 
 let emptyLines = {};
 emptyLines.horizontal = [];
 emptyLines.vertical = [];
 
 FindEmpty();
-//console.log(galaxies)
-Expand(1);
-//console.log(galaxies)
-
-//FindGalaxies();
 FindPaths();
 
+let sumBase = 0;
+let sumExp = 0;
+galaxies.forEach(galaxy => {
+    galaxy.shortestPaths.forEach(path => {
+        sumBase += path.baseLength ;
+        sumExp += path.expandingLines;
+    })
+})
+console.log(`Part 1: ${sumBase + sumExp} | ${sumBase + (sumExp * 999999)}`);
 
-let sum = 0;
-shortestPaths.forEach(path => sum+= path);
-console.log(sum);
 
 function FindEmpty()
 {
@@ -51,61 +47,26 @@ function FindEmpty()
     }
 }
 
-function Expand(amount = 1)
-{
-    for (let i = 0; i < emptyLines.horizontal.length; i++)
-    {
-        let value = emptyLines.horizontal[i];
-        for (let a = 0; a < amount; a++)
-        {
-            galaxyLines.splice(value, 0, '.'.repeat(galaxyLines[value].length));
-            emptyLines.horizontal.splice(i, 0, value)
-            galaxies.forEach(galaxy => {
-                if (galaxy.y > value) galaxy.y++;
-            })
-            for (let followingGalaxies = i + 1; followingGalaxies < emptyLines.horizontal.length; followingGalaxies++)
-            {
-                emptyLines.horizontal[followingGalaxies] ++;
-
-            }
-        }
-        i+=amount;
-    }
-
-    console.log("Expanded vertically")
-
-    for (let i = 0; i < emptyLines.vertical.length; i++)
-    {
-        let value = emptyLines.vertical[i];
-        for (let a = 0; a < amount; a++)
-        {
-            for (let j = 0; j < galaxyLines.length; j++)
-            {
-                galaxyLines[j] = galaxyLines[j].splice(value, 0, '.');
-            }
-
-            emptyLines.vertical.splice(i, 0, value);
-            galaxies.forEach(galaxy => {
-                if (galaxy.x > value) galaxy.x++;
-            })
-            for (let followingGalaxies = i + 1; followingGalaxies < emptyLines.vertical.length; followingGalaxies++)
-            {
-                emptyLines.vertical[followingGalaxies]++;
-            }
-        }
-        i+=amount;
-    }
-}
-
-
 function FindPaths()
 {
     for (let i = 0; i < galaxies.length; i++)
     {
+        galaxies[i].shortestPaths = [];
         for (let j = i + 1; j < galaxies.length; j++)
         {
-            //console.log(`Finding shortest path betwee galaxy ${i + 1} and galaxy ${j + 1}... ${FindPath(galaxies[i], galaxies[j])}`)
-            shortestPaths.push(FindPath(galaxies[i], galaxies[j]));
+            let pathdata = {baseLength:FindPath(galaxies[i], galaxies[j]), expandingLines: 0};
+            pathdata.from = `(${galaxies[i].x}, ${galaxies[i].y})`;
+            pathdata.to = `(${galaxies[j].x}, ${galaxies[j].y})`;
+
+            emptyLines.horizontal.forEach(index => {
+                if ((index > galaxies[i].y && index < galaxies[j].y) || (index < galaxies[i].y && index > galaxies[j].y))
+                pathdata.expandingLines++;
+            })
+            emptyLines.vertical.forEach(index => {
+                if ((index > galaxies[i].x && index < galaxies[j].x) || (index < galaxies[i].x && index > galaxies[j].x))
+                pathdata.expandingLines++;
+            })
+            galaxies[i].shortestPaths.push(pathdata);
         }
     }
 }
