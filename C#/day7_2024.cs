4b305b2ce97bbdfef5fe3065de51f7e2504e4892
Daniel Moss <danielsmoss96@gmail.com>
2024-12-09 00:39:49 +0000

    More like permEWtation

--- C#/day7_2024.cs
+++ C#/day7_2024.cs
@@ -8,6 +8,7 @@ public static class Day7_2024
         string[] lines = rawInput.Split('\n', StringSplitOptions.TrimEntries);
 
         long part1 = 0;
+        long part2 = 0;
 
         foreach (var line in lines)
         {
@@ -22,11 +23,15 @@ public static class Day7_2024
 
             Entry entry = new Entry(value, components);
 
-            if (entry.ValidateOperations() > 0)
+            (int a, int b) = entry.ValidateOperations();
+
+            if (a > 0)
                 part1 += entry.Value;
+            if (b > 0)
+                part2 += entry.Value;
         }
         
-        Console.WriteLine($"Part 1: {part1}");
+        Console.WriteLine($"Part 1: {part1} | Part 2: {part2}");
     }
 
     public class Entry
@@ -35,26 +40,6 @@ public static class Day7_2024
         public List<int> Components;
 
         private int NumberOfOperations;
-
-        [Flags]
-        internal enum Operations
-        {
-            FirstMulti = 1 << 0,
-            SecondMulti = 1 << 1,
-            ThirdMulti = 1 << 2,
-            FourthMulti = 1 << 3,
-            FifthMulti = 1 << 4,
-            SixthMulti = 1 << 5,
-            SeventhMulti = 1 << 6,
-            EighthMulti = 1 << 7,
-            NinethMulti = 1 << 8,
-            TenthMulti = 1 << 9,
-            EleventhMulti = 1 << 10,
-            TwelvethMulti = 1 << 11,
-            ThirteenthMulti = 1 << 12,
-            FourteenthMulti = 1 << 13,
-            FifteenthMulti = 1 << 14,
-        }
 
         public Entry(long value, List<int> components)
         {
@@ -65,51 +50,43 @@ public static class Day7_2024
             NumberOfOperations = Components.Count - 1;
         }
 
-        public int ValidateOperations()
+        public (int a, int b) ValidateOperations()
         {
             int validSolutions = 0;
+            int validSolutionsWithConcat = 0;
+            
+            List<int> operations = new List<int>();
 
-            for (int i = 0; i < Math.Pow(2, NumberOfOperations); i++)
+            for (int i = 0; i < NumberOfOperations; i++)
             {
-                Operations operations = (Operations)i;
-                if (AnswerValid(operations))
-                    validSolutions++;
+                operations.Add(0);
             }
 
-            return validSolutions;
+            for (int i = 0; i <  NumberOfOperations; i++)
+            {
+                //TODO - make a proper permutable here
+                foreach (var perm in perms)
+                    if (AnswerValid(perm.ToList()))
+                        validSolutions++;
+            }
+
+            return (validSolutions, validSolutionsWithConcat);
         }
 
-        private bool AnswerValid(Operations operations)
+        private bool AnswerValid(List<int> operations)
         {
             long output = Components[0];
 
-            Dictionary<int, bool> multiplierLookup = new Dictionary<int, bool>()
-            {
-                { 1, operations.HasFlag(Operations.FirstMulti) },
-                { 2, operations.HasFlag(Operations.SecondMulti) },
-                { 3, operations.HasFlag(Operations.ThirdMulti) },
-                { 4, operations.HasFlag(Operations.FourthMulti) },
-                { 5, operations.HasFlag(Operations.FifthMulti) },
-                { 6, operations.HasFlag(Operations.SixthMulti) },
-                { 7, operations.HasFlag(Operations.SeventhMulti) },
-                { 8, operations.HasFlag(Operations.EighthMulti) },
-                { 9, operations.HasFlag(Operations.NinethMulti) },
-                { 10, operations.HasFlag(Operations.TenthMulti) },
-                { 11, operations.HasFlag(Operations.EleventhMulti) },
-                { 12, operations.HasFlag(Operations.TwelvethMulti) },
-                { 13, operations.HasFlag(Operations.ThirteenthMulti) },
-                { 14, operations.HasFlag(Operations.FourteenthMulti) },
-                { 15, operations.HasFlag(Operations.FifteenthMulti) }
-            };
-
             for (int i = 1; i < Components.Count; i++)
             {
-                if (multiplierLookup[i])
+                if (operations[i-1] == 1)
+                    output += Components[i];
+                else if (operations[i-1] == 2)
                     output *= Components[i];
                 else
-                    output += Components[i];
+                    output = long.Parse($"{output}{Components[i]}");
             }
-            
+
             return output == Value;
         }
     }
